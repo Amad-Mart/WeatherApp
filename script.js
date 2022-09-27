78ceda148ca6dfff07cf39e7605264d671f262b2
Deus <amadeusrm97@gmail.com>
2022-09-27 19:37:26 -0400

    displayWeather funciton implemented

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 let weather = {
     'apikey': '6102270d2b0957cfcfe47dbcb4c614ad',
-    fetchWeather: function (){
+    fetchWeather: function (city){
         fetch(
-            'https://api.openweathermap.org/data/2.5/weather?q=buford&appid=6102270d2b0957cfcfe47dbcb4c614ad'
+            'https://api.openweathermap.org/data/2.5/weather?q='
+            + city
+            + '&units=imperial&appid='
+            + this.apikey
         )
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));
+        //.then((data) => console.log(data));
     },
+    displayWeather: function(data){
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector('.city').innerText = 'Weather in '+ name;
+        document.querySelector('.icon').src =
+        'https://openweathermap.org/img/wn/'+ icon +'.png';
+        document.querySelector('.temp').innerText = temp +'°F' 
+        document.querySelector('.description').innerText = description
+        document.querySelector('.humidity').innerText = 'Humidity: '+ humidity +' %'
+        document.querySelector('.wind').innerText = 'Wind speed: '+ speed + "mph"
+    }
 };
